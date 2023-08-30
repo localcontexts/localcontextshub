1c84cd35508c47c9b7091dd586a09e9c0b778c42
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-08-30 11:30:45 -0700

    test deploy 8

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #
 # This script is an entry point for the App Engine Standard deployment.
 #
-# The script first sets up the environment variables by executing the `env.sh` script
+# The script first sets up the environment variables by executing the env.sh script
 # and then starts the gunicorn server.
 #
 
