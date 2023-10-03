515ae03828ae4efa549d6ed0e16d839d28454888
sabeen <sabeen.ammar@localcontexts.org>
2023-10-03 04:09:38 -0400

    updated django engine

--- localcontexts/settings.py
+++ localcontexts/settings.py
@@ -120,7 +120,7 @@ if os.getenv('GAE_APPLICATION', None):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASS'),
