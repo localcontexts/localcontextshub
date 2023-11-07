e32ad79f7dbda4206ba2d6903d30a3684892b17b
lc_hd <lc_hd>
2023-11-06 21:16:48 -0500

    feat: created placeholder testcases for unit and functional tests

---
+++ tests/functional/test_functional_example.py
@@ -0,0 +1,5 @@
+# Use this to test non-unit tests
+
+
+def test_placeholder():
+    print('Functional Test Place Holder')
---
+++ tests/unit/test_unit_example.py
@@ -0,0 +1,5 @@
+# Use this to test custom functions
+
+
+def test_placeholder():
+    print('Unit Test Place Holder')
