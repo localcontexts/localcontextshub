742e9be473614ad42cda36014db08a6fd1ac1e81
sabeen <sabeen.ammar@localcontexts.org>
2024-01-19 11:43:06 -0600

    Added Custom message for the Google auth user

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,6 +1,8 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.account import signals
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 
 class CustomAccountAdapter(DefaultAccountAdapter):
@@ -11,6 +13,36 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def respond_user_inactive(self, request, user):
         messages.error(request, 'Your username or password does not match an account')
         return redirect('login')
+    
+    def post_login(
+        self,
+        request,
+        user,
+        *,
+        email_verification,
+        signal_kwargs,
+        email,
+        signup,
+        redirect_url
+    ):
+        from allauth.account.utils import get_login_redirect_url
+
+        response = HttpResponseRedirect(
+            get_login_redirect_url(request, redirect_url, signup=signup)
+        )
+        
+        if signal_kwargs is None:
+            signal_kwargs = {}
+        signals.user_logged_in.send(
+            sender=user.__class__,
+            request=request,
+            response=response,
+            user=user,
+            **signal_kwargs,
+        )
+        custom_message = ("Successfully signed in as {} using Google Auth").format(user.get_username())
+        messages.add_message(request, messages.SUCCESS, custom_message)
+        return response
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     
