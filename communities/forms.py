fe5221e463a900942147f18e1ca984237dac7754
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2024-01-24 14:25:10 -0800

    remove contact name and email from update account forms and settings template, update settings tab, fix template tag error in researcher-card

--- communities/forms.py
+++ communities/forms.py
@@ -153,11 +153,9 @@ class CommunityModelForm(forms.ModelForm):
 class UpdateCommunityForm(forms.ModelForm):
     class Meta:
         model = Community
-        fields = ['contact_name', 'contact_email', 'description', 'community_entity', 'city_town', 'state_province_region', 'country', 'image']
+        fields = ['description', 'community_entity', 'city_town', 'state_province_region', 'country', 'image']
         widgets = {
             'community_entity': forms.TextInput(attrs={'class': 'w-100'}),
-            'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
-            'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
             'state_province_region': forms.TextInput(attrs={'class': 'w-100'}),
             'city_town': forms.TextInput(attrs={'class': 'w-100'}),
             'description': forms.Textarea(attrs={'rows': 3, 'class': 'w-100'}),
