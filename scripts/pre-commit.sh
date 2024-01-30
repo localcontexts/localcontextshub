c051f645815fef70a28bbd3558d4b7f3535cd0a2
lc_hd <herbert.dawkins@localcontexts.org>
2024-01-30 15:48:56 -0500

    feat: modified pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,3 +1,9 @@
-# these are run during the git pre-commit
+echo -e 'FORMATTING PYTHON FILES'
+# this makes eligible files PEP8 compliant automatically
 yapf --recursive -i .
+echo -e 'FORMATTING PYTHON FILES: COMPLETED\n\n'
+
+echo -e 'RUNNING PYTHON LINTER\n'
+# this shows us which files are not PEP8 compliant
 flake8
+echo -e '\n\nRUNNING PYTHON LINTER: COMPLETED'
