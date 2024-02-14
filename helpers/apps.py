02882a5ba907cc0d47c3acce3b1ffe39d5fb2d92
sabeen <sabeen.ammar@localcontexts.org>
2024-02-14 11:20:49 -0500

    Added schduler to delete inactive users

--- helpers/apps.py
+++ helpers/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class HelpersConfig(AppConfig):
     name = 'helpers'
+
+    def ready(self):
+        from helpers.scheduler import start_scheduler
+        start_scheduler()
