a7be1cebc5af7c0c3dfea7c67671c842ce659f6b
sab-LC <145371882+sab-LC@users.noreply.github.com>
2024-02-19 14:45:12 -0600

    #8687362uf Action Notifications: auto-delete if event instance gets deleted (#100)
    * added signals for labels and members
    
    * Updated the action notification deletion on project deletion
    
    * Improved code quality

---
+++ notifications/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from .models import ActionNotification
+from tklabels.models import TKLabel
+from bclabels.models import BCLabel
+from communities.models import JoinRequest
+from projects.models import Project
+
+@receiver(post_delete, sender=TKLabel)
+@receiver(post_delete, sender=BCLabel)
+@receiver(post_delete, sender=Project)
+def delete_related_notifications(sender, instance, **kwargs):
+    ActionNotification.objects.filter(reference_id=str(instance.unique_id)).delete()
+
+@receiver(post_delete, sender=JoinRequest)
+def delete_related_notifications(sender, instance, **kwargs):
+    ActionNotification.objects.filter(reference_id=str(instance.id)).delete()
