42aaecb27f5dc7110577335a13bc714cf5a226d6
lc_hd <herbert.dawkins@localcontexts.org>
2024-03-08 10:31:09 -0500

    feat: created pre-commit.sh file

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo 'FORMATTING PYTHON FILES'
+# this makes eligible files PEP8 compliant automatically
+yapf --recursive -i .
+echo 'FORMATTING PYTHON FILES: COMPLETED'
+
+echo '------------------------------------------------------------'
+
+echo 'RUNNING PYTHON LINTER'
+# this shows us which files are not PEP8 compliant
+flake8
+echo 'RUNNING PYTHON LINTER: COMPLETED'
