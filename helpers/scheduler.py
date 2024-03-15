cee3bb9fcfc9fd0c5c39a463a3648e76e9ed5426
sabeen <sabeen.ammar@localcontexts.org>
2024-03-15 10:42:06 -0400

    reverted id

--- helpers/scheduler.py
+++ helpers/scheduler.py
@@ -18,7 +18,7 @@ def start_scheduler():
     scheduler.add_job(
         cleanup_inactive_users,
         trigger=CronTrigger(hour='*/23'),
-        id="cleanup_inactive_users_24h",
+        id="cleanup_inactive_users",
         max_instances=1,
         replace_existing=True,
     )
