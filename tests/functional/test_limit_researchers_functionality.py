50a8f8252d2daff41f9ac8e6ff576a6a1d25559f
lc_hd <herbert.dawkins@localcontexts.org>
2024-04-15 17:36:08 -0400

    feat: created testcases

---
+++ tests/functional/test_limit_researchers_functionality.py
@@ -0,0 +1,75 @@
+from django.urls import reverse
+from django.test import Client, TransactionTestCase
+from factories.researchers_factories import ResearcherFactory
+from factories.accounts_factories import UserFactory
+from factories.projects_factories import ProjectFactory
+
+from helpers.exceptions import UnsubscribedAccountException
+
+
+class TestFeatures(TransactionTestCase):
+    def setUp(self):
+        self.client = Client()
+        self.subscribed_researcher = ResearcherFactory(user=UserFactory(), is_subscribed=True)
+        self.subscribed_researcher_project = ProjectFactory(project_creator=self.subscribed_researcher.user)
+        self.unsubscribed_researcher = ResearcherFactory(user=UserFactory(), is_subscribed=False)
+        self.unsubscribed_researcher_project = ProjectFactory(project_creator=self.unsubscribed_researcher.user)
+
+    def test_download_otc_as_subscribed(self):
+        self.client.force_login(user=self.subscribed_researcher.user)
+        kwargs = {
+            'perm': 1,
+            'researcher_id': self.subscribed_researcher.id
+        }
+        response = self.client.get(reverse('download-open-to-collaborate-notice-researcher', kwargs=kwargs))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('open-to-collaborate-notice', response.url)
+
+    def test_download_otc_as_unsubscribed(self):
+        self.client.force_login(user=self.unsubscribed_researcher.user)
+        kwargs = {
+            'perm': 1,
+            'researcher_id': self.unsubscribed_researcher.id
+        }
+        response = self.client.get(reverse('download-open-to-collaborate-notice-researcher', kwargs=kwargs))
+        self.assertEqual(response.status_code, 403)
+
+    def test_view_project_as_subscribed(self):
+        self.client.force_login(user=self.subscribed_researcher.user)
+        kwargs = {
+            'unique_id': self.subscribed_researcher_project.unique_id,
+        }
+        response = self.client.get(reverse('view-project', kwargs=kwargs))
+        self.assertEqual(response.context['can_download'], True)
+
+    def test_create_project_as_unsubscribed(self):
+        self.client.force_login(user=self.unsubscribed_researcher.user)
+        kwargs = {
+            'pk': self.unsubscribed_researcher.id,
+        }
+        response = self.client.get(reverse('researcher-create-project', kwargs=kwargs))
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_project_as_subscribed(self):
+        self.client.force_login(user=self.subscribed_researcher.user)
+        kwargs = {
+            'pk': self.subscribed_researcher.id,
+        }
+        response = self.client.get(reverse('researcher-create-project', kwargs=kwargs))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_project_as_unsubscribed(self):
+        self.client.force_login(user=self.unsubscribed_researcher.user)
+        kwargs = {
+            'pk': self.unsubscribed_researcher.id,
+        }
+        response = self.client.get(reverse('researcher-create-project', kwargs=kwargs))
+        self.assertEqual(response.status_code, 403)
+
+    def test_edit_project_as_subscribed(self):
+        self.client.force_login(user=self.subscribed_researcher.user)
+        kwargs = {
+            'pk': self.subscribed_researcher.id,
+        }
+        response = self.client.get(reverse('researcher-create-project', kwargs=kwargs))
+        self.assertEqual(response.status_code, 200)
