c7cf9ae82d0c65e68698cce8a1c21219f2138e75
sabeen <sabeen.ammar@localcontexts.org>
2024-05-10 15:09:27 -0500

    Solved no first name and last name issue in researher creation

--- researchers/views.py
+++ researchers/views.py
@@ -88,6 +88,20 @@ def connect_researcher(request):
                 if subscription_form.is_valid():
                     handle_confirmation_and_subscription(request, subscription_form, data)
                     return redirect('dashboard')
+                else:
+                    error_messages = []
+                    for field, errors in subscription_form.errors.items():
+                        for error in errors:
+                            error_messages.append(f"{field.capitalize()}: {error}")
+
+                    # Join error messages into a single string
+                    concatenated_errors = "\n".join(error_messages)
+                    messages.add_message(
+                    request,
+                    messages.ERROR,
+                    concatenated_errors,
+                )
+                    return redirect('confirm-subscription-researcher', data.id)
         context = {'form': form, 'env': env, 'subscription_form': subscription_form,}
         return render(request, 'researchers/connect-researcher.html', context)
     else:
@@ -159,6 +173,64 @@ def public_researcher_view(request, pk):
     except:
         raise Http404()
 
+@login_required(login_url="login")
+def confirm_subscription_researcher(request, pk):
+    join_flag = False
+    researcher = get_object_or_404(Researcher, id=pk)
+    initial_data = {
+        "first_name": request.user._wrapped.first_name,
+        "last_name": request    .user._wrapped.last_name,
+        "email": request.user._wrapped.email,
+        "account_type": "researcher_account",
+        "organization_name": request.user._wrapped.first_name,
+    }
+    modified_account_type_choices = [
+        choice
+        for choice in SubscriptionForm.INQUIRY_TYPE_CHOICES
+        if choice[0] != "member"
+    ]
+    form = SubscriptionForm(request.POST or None, initial=initial_data)
+    form.fields["inquiry_type"].choices = modified_account_type_choices
+    form.fields["account_type"].widget.attrs.update({"class": "w-100 readonly-input"})
+    form.fields["organization_name"].widget.attrs.update({"class": "readonly-input"})
+    form.fields["email"].widget.attrs.update({"class": "readonly-input"})
+    if request.method == "POST":
+        if validate_recaptcha(request) and form.is_valid():
+            account_type_key = form.cleaned_data["account_type"]
+            inquiry_type_key = form.cleaned_data["inquiry_type"]
+
+            account_type_display = dict(form.fields["account_type"].choices).get(
+                account_type_key, ""
+            )
+            inquiry_type_display = dict(form.fields["inquiry_type"].choices).get(
+                inquiry_type_key, ""
+            )
+            form.cleaned_data["account_type"] = account_type_display
+            form.cleaned_data["inquiry_type"] = inquiry_type_display
+
+            first_name = form.cleaned_data["first_name"]
+            if not form.cleaned_data["last_name"]:
+                form.cleaned_data["last_name"] = first_name
+            try:
+                response = confirm_subscription(request, researcher, join_flag, form)
+                return response
+            except:
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    "An unexpected error has occurred here. Please contact support@localcontexts.org.",
+                )
+                return redirect("dashboard")
+    return render(
+        request,
+        "accounts/confirm-subscription.html",
+        {
+            "form": form,
+            "account": researcher,
+            "subscription_url": 'confirm-subscription-researcher',
+            "join_flag": join_flag,
+        },
+    )
 
 @login_required(login_url='login')
 def connect_orcid(request):
