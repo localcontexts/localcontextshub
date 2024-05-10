84510414aba7602bbbd3003c6be4fd5f57a5ca25
Ashley Rojas <ashley.rojas95@gmail.com>
2024-05-10 16:55:28 -0400

    added exception for non-subscribed accounts in template

--- institutions/views.py
+++ institutions/views.py
@@ -1541,53 +1541,58 @@ def api_keys(request, pk, related=None):
     institution = get_institution(pk)
     member_role = check_member_role(request.user, institution)
     subscription_api_key_count = 0
-    subscription = Subscription.objects.get(institution=institution)
-    subscription_api_key_count = subscription.api_key_count
     
+    try:
+        if institution.is_subscribed:
+                subscription = Subscription.objects.get(institution=institution)
+                subscription_api_key_count = subscription.api_key_count
+                
+        if request.method == 'GET':
+            form = APIKeyGeneratorForm(request.GET or None)
+            institution_keys = AccountAPIKey.objects.filter(institution=institution).values_list("prefix", "name", "encrypted_key")
     
-    if request.method == 'GET':
-        form = APIKeyGeneratorForm(request.GET or None)
-        institution_keys = AccountAPIKey.objects.filter(institution=institution).values_list("prefix", "name", "encrypted_key")
-    elif request.method == "POST":
-        if "generate_api_key" in request.POST:
-            if subscription.api_key_count == 0:
-                messages.add_message(request, messages.ERROR, 'Your institution has reached its API Key limit. '
-                                    'Please upgrade your subscription plan to create more API Keys.')
+        elif request.method == "POST":
+            if "generate_api_key" in request.POST:
+                if subscription.api_key_count == 0:
+                    messages.add_message(request, messages.ERROR, 'Your institution has reached its API Key limit. '
+                                        'Please upgrade your subscription plan to create more API Keys.')
+                    return redirect("institution-api-key", institution.id)
+                form = APIKeyGeneratorForm(request.POST)
+
+                if form.is_valid():
+                    data = form.save(commit=False)
+                    api_key, key = AccountAPIKey.objects.create_key(
+                        name = data.name,
+                        institution_id = institution.id
+                    )
+                    prefix = key.split(".")[0]
+                    encrypted_key = urlsafe_base64_encode(force_bytes(key))
+                    AccountAPIKey.objects.filter(prefix=prefix).update(encrypted_key=encrypted_key)
+
+                    if subscription.api_key_count > 0:
+                        subscription.api_key_count -= 1
+                        subscription.save()
+
                 return redirect("institution-api-key", institution.id)
-            form = APIKeyGeneratorForm(request.POST)
+            
+            elif "delete_api_key" in request.POST:
+                prefix = request.POST['delete_api_key']
+                api_key = AccountAPIKey.objects.filter(prefix=prefix)
+                api_key.delete()
 
-            if form.is_valid():
-                data = form.save(commit=False)
-                api_key, key = AccountAPIKey.objects.create_key(
-                    name = data.name,
-                    institution_id = institution.id
-                )
-                prefix = key.split(".")[0]
-                encrypted_key = urlsafe_base64_encode(force_bytes(key))
-                AccountAPIKey.objects.filter(prefix=prefix).update(encrypted_key=encrypted_key)
-
-                if subscription.api_key_count > 0:
-                    subscription.api_key_count -= 1
+                if subscription.api_key_count >= 0:
+                    subscription.api_key_count +=1
                     subscription.save()
 
-            return redirect("institution-api-key", institution.id)
-        
-        elif "delete_api_key" in request.POST:
-            prefix = request.POST['delete_api_key']
-            api_key = AccountAPIKey.objects.filter(prefix=prefix)
-            api_key.delete()
+                return redirect("institution-api-key", institution.id)
 
-            if subscription.api_key_count >= 0:
-                subscription.api_key_count +=1
-                subscription.save()
-
-            return redirect("institution-api-key", institution.id)
-
-    context = {
-        "institution" : institution,
-        "form" : form,
-        "institution_keys" : institution_keys,
-        "member_role" : member_role,
-        "subscription_api_key_count" : subscription_api_key_count
-    }
-    return render(request, 'account_settings_pages/_api-keys.html', context)
+        context = {
+            "institution" : institution,
+            "form" : form,
+            "institution_keys" : institution_keys,
+            "member_role" : member_role,
+            "subscription_api_key_count" : subscription_api_key_count
+        }
+        return render(request, 'account_settings_pages/_api-keys.html', context)
+    except:
+        raise Http404()
