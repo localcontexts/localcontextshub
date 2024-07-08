670b1f2b0d07da5fd35c1cfb7514230241233207
lc_hd <herbert.dawkins@localcontexts.org>
2024-07-08 07:10:14 -0400

    feat: created options for toastr alert

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -2285,3 +2285,22 @@ if (window.location.href.includes('/institutions/update/') || window.location.hr
         }
     }
  }
+
+ // options for toastr alert
+ toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
