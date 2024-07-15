e84e43989fb58c05b9ed89cf814bc91e5dda25e6
lc_hd <herbert.dawkins@localcontexts.org>
2024-07-14 21:55:28 -0400

    feat: reformatted urls file

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path('read/<str:pk>/', views.read_notification, name="read"),
     path(
-    'user-notification/delete/<str:pk>/',
-    views.delete_user_notification,
-    name="delete-user-notification"
+        'user-notification/delete/<str:pk>/',
+        views.delete_user_notification,
+        name="delete-user-notification"
     ),
     path(
-    'organization/read/<str:pk>',
-    views.read_org_notification,
-    name="org-read"
+        'organization/read/<str:pk>',
+        views.read_org_notification,
+        name="org-read"
     ),
 ]
