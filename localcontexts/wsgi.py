8a2320921d386dee08d733ac60be21ceccf70506
sabeen <sabeen.ammar@localcontexts.org>
2024-08-06 08:34:02 -0400

    Updated the wsgi settings

--- localcontexts/wsgi.py
+++ localcontexts/wsgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localcontexts.settings') 
 
 from django.core.wsgi import get_wsgi_application
 
 # Import your start_scheduler function
 from helpers.scheduler import start_scheduler
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localcontexts.settings')
 
 application = get_wsgi_application()
 
