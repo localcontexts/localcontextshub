b4b5221c54d1086736e5e483bbb19441ceffc944
lc_hd <herbert.dawkins@localcontexts.org>
2024-09-11 15:50:46 -0400

    feat: created testcase for test_user_created_project_in_institution

--- tests/unit/test_custom_acct_tags.py
+++ tests/unit/test_custom_acct_tags.py
@@ -1,10 +1,14 @@
 from django.test import TransactionTestCase
 from factories.accounts_factories import UserFactory
 from factories.communities_factories import CommunityFactory
+from factories.institutions_factories import InstitutionFactory
 from factories.projects_factories import ProjectCreatorFactory
 from factories.projects_factories import ProjectFactory
 
-from accounts.templatetags.custom_acct_tags import user_created_project_in_community
+from accounts.templatetags.custom_acct_tags import (
+    user_created_project_in_community,
+    user_created_project_in_institution
+)
 
 
 class TestCustomAccountTags(TransactionTestCase):
@@ -31,4 +35,29 @@ class TestCustomAccountTags(TransactionTestCase):
         assert user_created_project_in_community(
             user_id=user.id,
             community_id=community_without_project.id
+        ) is False
+
+    def test_user_created_project_in_institution(self):
+        # create user and institutions
+        user = UserFactory()
+        institution_with_project = InstitutionFactory()
+        institution_without_project = InstitutionFactory()
+
+        # create project for the institution_with_project
+        project = ProjectFactory(
+            project_creator=user
+        )
+        ProjectCreatorFactory(
+            institution=institution_with_project,
+            project=project
+        )
+
+        assert user_created_project_in_institution(
+            user_id=user.id,
+            institution_id=institution_with_project.id
+        ) is True
+
+        assert user_created_project_in_institution(
+            user_id=user.id,
+            institution_id=institution_without_project.id
         ) is False
