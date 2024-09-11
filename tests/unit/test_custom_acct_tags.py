33a5808b09cf8588751b6730a97f61a1b6830654
lc_hd <herbert.dawkins@localcontexts.org>
2024-09-11 16:05:12 -0400

    feat: created testcase for test_user_created_project_as_researcher

--- tests/unit/test_custom_acct_tags.py
+++ tests/unit/test_custom_acct_tags.py
@@ -2,12 +2,13 @@ from django.test import TransactionTestCase
 from factories.accounts_factories import UserFactory
 from factories.communities_factories import CommunityFactory
 from factories.institutions_factories import InstitutionFactory
+from factories.researchers_factories import ResearcherFactory
 from factories.projects_factories import ProjectCreatorFactory
 from factories.projects_factories import ProjectFactory
 
 from accounts.templatetags.custom_acct_tags import (
     user_created_project_in_community,
-    user_created_project_in_institution
+    user_created_project_in_institution, user_created_project_as_researcher
 )
 
 
@@ -60,4 +61,29 @@ class TestCustomAccountTags(TransactionTestCase):
         assert user_created_project_in_institution(
             user_id=user.id,
             institution_id=institution_without_project.id
+        ) is False
+
+    def test_researcher_created_project(self):
+        # create user and researcher
+        user = UserFactory()
+        researcher_with_project = ResearcherFactory()
+        researcher_without_project = ResearcherFactory()
+
+        # create project for the researcher_with_project
+        project = ProjectFactory(
+            project_creator=user
+        )
+        ProjectCreatorFactory(
+            researcher=researcher_with_project,
+            project=project
+        )
+
+        assert user_created_project_as_researcher(
+            user_id=user.id,
+            researcher_id=researcher_with_project.id
+        ) is True
+
+        assert user_created_project_as_researcher(
+            user_id=user.id,
+            researcher_id=researcher_without_project.id
         ) is False
