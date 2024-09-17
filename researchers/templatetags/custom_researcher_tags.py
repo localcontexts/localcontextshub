159ff75e0176016d5abe72e5c1fad8c83542ca18
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2024-09-17 12:09:48 -0400

    flake8 formatting

--- researchers/templatetags/custom_researcher_tags.py
+++ researchers/templatetags/custom_researcher_tags.py
@@ -4,27 +4,38 @@ from projects.models import ProjectContributors, ProjectCreator
 
 register = template.Library()
 
+
 # @register.simple_tag
 # def anchor(url_name, section_id, researcher_id):
-#     return reverse(url_name, kwargs={'pk': researcher_id}) + "#project-unique-" + str(section_id)
+#     return reverse(url_name, kwargs={
+# 'pk': researcher_id}) + "#project-unique-" + str(section_id)
+
 
 @register.simple_tag
 def get_notices_count(researcher):
     return Notice.objects.filter(researcher=researcher).count()
 
+
 @register.simple_tag
 def get_labels_count(researcher):
     count = 0
-    for instance in ProjectCreator.objects.select_related('project').prefetch_related('project__bc_labels', 'project__tk_labels').filter(researcher=researcher):
+    for instance in ProjectCreator.objects.select_related(
+        'project'
+        ).prefetch_related(
+            'project__bc_labels', 'project__tk_labels'
+            ).filter(researcher=researcher):
         if instance.project.has_labels():
             count += 1
     return count
+
 
 @register.simple_tag
 def researcher_contributing_projects(researcher):
     return ProjectContributors.objects.select_related('project').filter(researchers=researcher)
 
+
 @register.simple_tag
 def connections_count(researcher):
-    contributor_ids = researcher.contributing_researchers.exclude(communities__id=None).values_list('communities__id', flat=True)
+    contributor_ids = researcher.contributing_researchers.exclude(
+        communities__id=None).values_list('communities__id', flat=True)
     return len(contributor_ids)
