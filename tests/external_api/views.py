d6f51fd7b2d48eab66d944c5f06771e9038caacd
lc_hd <herbert.dawkins@localcontexts.org>
2024-09-20 02:00:13 -0400

    feat: updated mocked external_api to serve the native_land boundary response

--- tests/external_api/views.py
+++ tests/external_api/views.py
@@ -7,3 +7,9 @@ def native_land_map_list(request):
     json_data = open('tests/fixtures/native_land_map_list.json')
     data = json.load(json_data)
     return JsonResponse(data, safe=False)
+
+
+def native_land_boundary_response(request):
+    json_data = open('tests/fixtures/native_land_boundary_response.json')
+    data = json.load(json_data)
+    return JsonResponse(data, safe=False)
