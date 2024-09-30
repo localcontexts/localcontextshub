39fbe569b41aa2e495634d80af347affe56cc2ac
lc_hd <herbert.dawkins@localcontexts.org>
2024-09-30 12:09:59 -0400

    feat: added similar testcase but for institutions

--- tests/functional/test_preview_boundary_ui.py
+++ tests/functional/test_preview_boundary_ui.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from functional.ui_feature_testcase_base import UiFeatureHelper
 from factories.projects_factories import ProjectFactory
 from communities.models import Community, Boundary
+from institutions.models import Institution
 from projects.models import Project
 
 
@@ -19,8 +20,10 @@ class TestBoundaryPreviewFeatures(UiFeatureHelper):
         self.original_boundary_coordinates = [
                 [0, 0], [0, 1], [0, 2]
         ]
-        self.project = None
+        self.community_project = None
+        self.institution_project = None
         self.community = None
+        self.institution = None
 
     def accept_cookies(self):
         # this removes the accept-cookies overlay so other
@@ -41,16 +44,34 @@ class TestBoundaryPreviewFeatures(UiFeatureHelper):
             coordinates=self.original_boundary_coordinates
         )
         boundary.save()
-        self.project = ProjectFactory(
+        self.community_project = ProjectFactory(
             project_creator=self.user,
             boundary=boundary,
             source_of_boundary=self.original_source_of_boundary,
             name_of_boundary=self.original_name_of_boundary,
             urls=[]
         )
-        self.project.save()
+        self.community_project.save()
 
-    def confirm_preexisting_boundary_was_not_overwritten(self):
+    def create_project_and_institution(self):
+        self.institution = Institution(
+            institution_creator=self.user,
+        )
+        self.institution.save()
+        boundary = Boundary(
+            coordinates=self.original_boundary_coordinates
+        )
+        boundary.save()
+        self.institution_project = ProjectFactory(
+            project_creator=self.user,
+            boundary=boundary,
+            source_of_boundary=self.original_source_of_boundary,
+            name_of_boundary=self.original_name_of_boundary,
+            urls=[]
+        )
+        self.institution_project.save()
+
+    def confirm_preexisting_project_boundary_was_not_overwritten_for_community(self):
         # confirm in the UI on project actions view page
         boundary_title = self.py.get("[data-test-id='native-land-link']")
         boundary_title.should().be_visible()
@@ -62,12 +83,29 @@ class TestBoundaryPreviewFeatures(UiFeatureHelper):
         assert self.original_source_of_boundary in boundary_url_source, 'Unexpected Url'
 
         # reload project and confirm its boundary coordinates are the same
-        reloaded_project = Project.objects.get(unique_id=self.project.unique_id)
+        reloaded_project = Project.objects.get(unique_id=self.community_project.unique_id)
         reloaded_boundary_coordinates = reloaded_project.boundary.get_coordinates()
-        original_boundary_coordinates = self.project.boundary.get_coordinates()
+        original_boundary_coordinates = self.community_project.boundary.get_coordinates()
         assert original_boundary_coordinates == reloaded_boundary_coordinates, 'Unexpected Coordinates'
 
-    def test_that_cleared_boundaries_are_not_saved(self):
+    def confirm_preexisting_project_boundary_was_not_overwritten_for_institution(self):
+        # confirm in the UI on project actions view page
+        boundary_title = self.py.get("[data-test-id='native-land-link']")
+        boundary_title.should().be_visible()
+        boundary_title.should().have_text(
+            self.original_name_of_boundary
+        )
+
+        boundary_url_source = boundary_title.get_attribute('href')
+        assert self.original_source_of_boundary in boundary_url_source, 'Unexpected Url'
+
+        # reload project and confirm its boundary coordinates are the same
+        reloaded_project = Project.objects.get(unique_id=self.institution_project.unique_id)
+        reloaded_boundary_coordinates = reloaded_project.boundary.get_coordinates()
+        original_boundary_coordinates = self.institution_project.boundary.get_coordinates()
+        assert original_boundary_coordinates == reloaded_boundary_coordinates, 'Unexpected Coordinates'
+
+    def test_that_cleared_project_boundaries_are_not_saved_for_a_community(self):
         self.create_project_and_community()
 
         # visit project edit page for community
@@ -76,7 +114,7 @@ class TestBoundaryPreviewFeatures(UiFeatureHelper):
                 'edit-project',
                 kwargs={
                     'pk': self.community.id,
-                    'project_uuid': self.project.unique_id,
+                    'project_uuid': self.community_project.unique_id,
                 }
             )
         )
@@ -100,4 +138,39 @@ class TestBoundaryPreviewFeatures(UiFeatureHelper):
         save_project_button = self.py.get("#submitProjectBtn")
         save_project_button.click()
 
-        self.confirm_preexisting_boundary_was_not_overwritten()
+        self.confirm_preexisting_project_boundary_was_not_overwritten_for_community()
+
+    def test_that_cleared_project_boundaries_are_not_saved_for_an_institution(self):
+        self.create_project_and_institution()
+
+        # visit project edit page for institution
+        project_url = urllib.parse.urljoin(
+            self.live_server_url, reverse(
+                'inst-edit-project',
+                kwargs={
+                    'pk': self.institution.id,
+                    'project_uuid': self.institution_project.unique_id,
+                }
+            )
+        )
+        self.py.visit(project_url)
+
+        # click accept cookies button
+        time.sleep(5)   # wait for accept banner to appear
+        self.accept_cookies()
+
+        # select NLD territory
+        selected_territory = 'Panamakas'
+        self.select_specific_nld_territory(selected_territory)
+
+        # clear boundary
+        time.sleep(5)   # wait for boundary selection to occur
+        clear_boundary_button = self.py.get("#cancel-btn")
+        clear_boundary_button.click()
+
+        # save page
+        time.sleep(5)   # wait for boundary selection to occur
+        save_project_button = self.py.get("#submitProjectBtn")
+        save_project_button.click()
+
+        self.confirm_preexisting_project_boundary_was_not_overwritten_for_institution()
