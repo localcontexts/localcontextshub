9352d53ebf6e3dceaf743b05bdf3a7f8d3367aad
lc_hd <herbert.dawkins@localcontexts.org>
2024-10-09 14:25:35 -0400

    feat: updated test to remove all slug data as an external_api

--- tests/external_api/urls.py
+++ tests/external_api/urls.py
@@ -1,14 +1,13 @@
 from localcontexts.urls import urlpatterns
 from django.urls import path
 
-from . views import (
-    native_land_map_list,
-    native_land_boundary_response
-)
+from . views import native_land_map_list
 
 
 class UrlsWithMockedExternalApi:
     urlpatterns = urlpatterns + [
-        path('biocodellc/localcontexts_json/main/data/nativeland_slug_name_list.json', native_land_map_list),
-        path('wp-json/nativeland/v1/api/index.php', native_land_boundary_response)
+        path(
+            'biocodellc/localcontexts_json/main/data/nativeland_slug_name_list.json',
+            native_land_map_list
+        ),
     ]
