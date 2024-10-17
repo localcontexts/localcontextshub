538c2091e433480503513ccb3b8a5d0a92d207b5
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2024-10-17 14:57:59 -0700

    fix timezone import error

--- researchers/views.py
+++ researchers/views.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
